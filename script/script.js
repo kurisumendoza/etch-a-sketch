1724c3c0624e54da9398dc7e09caedec2669d1dc
kurisu26 <creative.kristopher@gmail.com>
2023-10-02 18:33:00 +0800

    Make a div turn black on mouse hover

--- script/script.js
+++ script/script.js
@@ -2,6 +2,11 @@ let container = document.querySelector('.container');
 
 for (let i = 1; i < 257; i++) {
   const content = document.createElement('div');
-  content.setAttribute('class', `content no-${i}`);
+  content.setAttribute('class', `content`);
   container.appendChild(content);
 }
+
+
+const sketchpad = document.querySelectorAll('.content').forEach(item => {
+  item.addEventListener('mouseover', () => item.classList.add('sketch'))
+})
