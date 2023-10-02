ed0d8a9ecd8b3137a5e1de67d0e5693fdd1afe18
kurisu26 <creative.kristopher@gmail.com>
2023-10-02 19:22:49 +0800

    Add button to change the amount of squares per side

--- script/script.js
+++ script/script.js
@@ -1,12 +1,39 @@
-let container = document.querySelector('.container');
+const container = document.querySelector('.container');
 
-for (let i = 1; i < 257; i++) {
+for (let i = 0; i < 256; i++) {
   const content = document.createElement('div');
   content.setAttribute('class', `content`);
+  content.setAttribute('style', 'flex-basis: 6.25%');
   container.appendChild(content);
 }
 
+drawing();
 
-const sketchpad = document.querySelectorAll('.content').forEach(item => {
-  item.addEventListener('mouseover', () => item.classList.add('sketch'))
-})
+function drawing() {
+  const sketchpad = document.querySelectorAll('.content').forEach(item => {
+    item.addEventListener('mouseover', () => item.classList.add('sketch'))
+  })
+}
+
+const newSketchpad = document.querySelector('.new-sketchpad');
+
+newSketchpad.addEventListener('click', () => pixelAmount())
+
+function pixelAmount() {
+  let pixel = prompt('Enter amount of squares per side?', 'Max: 100');
+
+  if (pixel <= 100) {
+    while (container.hasChildNodes()) {
+      container.removeChild(container.firstChild);
+    }
+
+    for (let i = 0; i < pixel * pixel; i++) {
+      const content = document.createElement('div');
+      content.setAttribute('class', `content`);
+      content.setAttribute('style', `flex-basis: ${100/pixel}%`);
+      container.appendChild(content);
+    }
+  }
+
+  drawing();
+}
