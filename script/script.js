90f60a718d7e71f11311b1b06854f6e9b29fc8ae
kurisu26 <creative.kristopher@gmail.com>
2023-10-05 05:02:40 +0800

    Add a pogressive darkening effect every mouse hover

--- script/script.js
+++ script/script.js
@@ -10,8 +10,44 @@ for (let i = 0; i < 256; i++) {
 drawing();
 
 function drawing() {
-  const sketchpad = document.querySelectorAll('.content').forEach(item => {
-    item.addEventListener('mouseover', () => item.classList.add('sketch'))
+  const sketchpad = document.querySelectorAll('.content');
+  
+  sketchpad.forEach(item => {
+    item.addEventListener('mouseover', () => {
+      if (item.classList.contains('sketch-ten')) {
+        return;
+      } else if (item.classList.contains('sketch-nine')) {
+        item.classList.remove('sketch-nine');
+        item.classList.add('sketch-ten');
+      } else if (item.classList.contains('sketch-eight')) {
+        item.classList.remove('sketch-eight');
+        item.classList.add('sketch-nine');
+      } else if (item.classList.contains('sketch-seven')) {
+        item.classList.remove('sketch-seven');
+        item.classList.add('sketch-eight');
+      } else if (item.classList.contains('sketch-six')) {
+        item.classList.remove('sketch-six');
+        item.classList.add('sketch-seven');
+      } else if (item.classList.contains('sketch-five')) {
+        item.classList.remove('sketch-five');
+        item.classList.add('sketch-six');
+      } else if (item.classList.contains('sketch-four')) {
+        item.classList.remove('sketch-four');
+        item.classList.add('sketch-five');
+      } else if (item.classList.contains('sketch-three')) {
+        item.classList.remove('sketch-three');
+        item.classList.add('sketch-four');
+      } else if (item.classList.contains('sketch-two')) {
+        item.classList.remove('sketch-two');
+        item.classList.add('sketch-three');
+      } else if (item.classList.contains('sketch')) {
+        item.classList.remove('sketch');
+        item.classList.add('sketch-two');
+      } else {
+        item.classList.add('sketch');
+      }
+    }
+    )
   })
 }
 
